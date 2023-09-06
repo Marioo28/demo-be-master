cd1351683e2ab8b2ab1216440e6dcc1db3c59fd0
Marius Marius <salceanu_m@yahoo.com>
2023-09-06 21:06:10 +0300

    implement communication in repo - @CrossOrigin - with Angular

--- src/main/java/com/example/modelbe/dao/CountryRepository.java
+++ src/main/java/com/example/modelbe/dao/CountryRepository.java
@@ -3,7 +3,9 @@ package com.example.modelbe.dao;
 import com.example.modelbe.entity.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "countries", path = "countries")
 public interface CountryRepository extends JpaRepository<Country, Long> {
 }
--- src/main/java/com/example/modelbe/dao/StateRepository.java
+++ src/main/java/com/example/modelbe/dao/StateRepository.java
@@ -3,7 +3,10 @@ package com.example.modelbe.dao;
 import com.example.modelbe.entity.State;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "states", path = "states")
 public interface StateRepository extends JpaRepository<State, Long> {
 }
